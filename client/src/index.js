6e3b7a0daf69b37cfa24841011f42b186d463f0a
Xunxin Li <xli9400@Xunxins-MacBook-Pro.local>
2021-05-22 00:21:36 -0400

    Installed Some npm package and created reducer, action, helper files

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider}from "react-redux"
+import {createStore, applyMiddleware} from "redux"
+
+import reducer from "./reducer";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
