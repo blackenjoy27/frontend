e481c90dd4002a61d0d9f6523f76f2f7778d708c
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-05-25 10:09:48 -0500

    basic signup schema

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Contact from "./Contact";
 import Footer from "./Footer";
-import SignUp from './SignUp'
 
 import {Link} from "react-router-dom";
 
@@ -23,7 +22,6 @@ const HomePage = ()=>{
             </div>
             
             <Contact/>
-            <SignUp/>
             <Footer/>
         </div>
         
