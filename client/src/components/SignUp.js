577164a372685928620ca37d719dc3171d891b46
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-05-25 17:04:52 -0500

    material ui testing

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
+import styled from 'styled-components';
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import LocationSearchingIcon from "@material-ui/icons/LocationSearching";
-import { IconButton } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -17,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
       marginTop: theme.spacing(8),
       display: "flex",
       flexDirection: "column",
-      alignItems: "center"
+      alignItems: "center",
     },
     avatar: {
       margin: theme.spacing(1),
@@ -32,14 +31,15 @@ const useStyles = makeStyles((theme) => ({
     }
   }));
 
-// const FormDiv = styled.div`
-//     background-image: url('https://hips.hearstapps.com/ell.h-cdn.co/assets/15/52/1450738482-elle-potluck-04-getty.jpg');
-//     background-repeat: no-repeat;
-//     background-position: bottom; 
-//     background-size: cover;
-//     color: white;
-//     padding: 15%;
-// `
+const FormContainer = styled.div`
+    background-image: url('https://hips.hearstapps.com/ell.h-cdn.co/assets/15/52/1450738482-elle-potluck-04-getty.jpg');
+    opacity: 0.5;
+    background-repeat: no-repeat;
+    background-position: bottom; 
+    background-size: cover;
+    color: white;
+    padding: 15%;
+`
 
 const initialValues = {
     username: "",
@@ -66,77 +66,78 @@ export default function SignUp() {
       };
 
     return (
+        <FormContainer style={{backgroundColor: '#4f4f4f'}}>
         <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} onSubmit={onSubmit}>
-          <Grid container spacing={2}>
-          <Grid item xs={12}>
-              <TextField
-                name="email"
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                type="email"
-                label="Email Address"
-                onChange={onChange}
-                value={values.email}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name="username"
-                variant="outlined"
-                required
-                fullWidth
-                onChange={onChange}
-                id="username"
-                label="Username"
-                value={values.username}
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name="password"
-                variant="outlined"
-                required
-                fullWidth
-                id="password"
-                type="password"
-                label="Password"
-                onChange={onChange}
-                value={values.password}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? login
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
-
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Create an Account
+                </Typography>
+                <form className={classes.form} onSubmit={onSubmit}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                name="email"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                type="email"
+                                label="Email Address"
+                                onChange={onChange}
+                                value={values.email}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                name="username"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                onChange={onChange}
+                                id="username"
+                                label="Username"
+                                value={values.username}
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                name="password"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="password"
+                                type="password"
+                                label="Password"
+                                onChange={onChange}
+                                value={values.password}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                        <Link href="#" variant="body3">
+                            Already have an account? Sign in
+                        </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
+        </FormContainer>
 
         // <Container component='main' maxWidth ='xs'>
         //     <form id = 'signup-form'>
