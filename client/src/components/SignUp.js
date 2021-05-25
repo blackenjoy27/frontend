991c7fdc7a417410f0ccb51f533f51577adbb7cd
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-05-25 18:56:56 -0500

    continued signup styling

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -33,12 +33,12 @@ const useStyles = makeStyles((theme) => ({
 
 const FormContainer = styled.div`
     background-image: url('https://hips.hearstapps.com/ell.h-cdn.co/assets/15/52/1450738482-elle-potluck-04-getty.jpg');
-    opacity: 0.5;
     background-repeat: no-repeat;
-    background-position: bottom; 
+    background-position: top; 
+    background-attachment: fixed;
     background-size: cover;
-    color: white;
-    padding: 15%;
+    color: #4f4f4f;
+    padding: 20%;
 `
 
 const initialValues = {
@@ -66,127 +66,76 @@ export default function SignUp() {
       };
 
     return (
-        <FormContainer style={{backgroundColor: '#4f4f4f'}}>
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Create an Account
-                </Typography>
-                <form className={classes.form} onSubmit={onSubmit}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                name="email"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                type="email"
-                                label="Email Address"
-                                onChange={onChange}
-                                value={values.email}
-                            />
+        <FormContainer>
+            <Container component="main" maxWidth="xs" style={{backgroundColor: 'white', paddingTop: '.1rem', paddingBottom: '3%', paddingLeft: '3%', paddingRight: '3%', borderRadius: '3%'}}>
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Create an Account
+                    </Typography>
+                    <form className={classes.form} onSubmit={onSubmit}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    name="email"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    type="email"
+                                    label="Email Address"
+                                    onChange={onChange}
+                                    value={values.email}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    name="username"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    onChange={onChange}
+                                    id="username"
+                                    label="Username"
+                                    value={values.username}
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    name="password"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="password"
+                                    type="password"
+                                    label="Password"
+                                    onChange={onChange}
+                                    value={values.password}
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                name="username"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                onChange={onChange}
-                                id="username"
-                                label="Username"
-                                value={values.username}
-                                autoFocus
-                            />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}>
+                            Sign Up
+                        </Button>
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                            <Link href="#" variant="body3">
+                                Already have an account? Sign in
+                            </Link>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                name="password"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="password"
-                                type="password"
-                                label="Password"
-                                onChange={onChange}
-                                value={values.password}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Sign Up
-                    </Button>
-                    <Grid container justify="flex-end">
-                        <Grid item>
-                        <Link href="#" variant="body3">
-                            Already have an account? Sign in
-                        </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-        </Container>
+                    </form>
+                </div>
+            </Container>
         </FormContainer>
-
-        // <Container component='main' maxWidth ='xs'>
-        //     <form id = 'signup-form'>
-        //         <div className = 'form-group submit'>
-        //             <h3>Sign up to join a Potluck, or host your own!</h3>
-        //         </div>
-
-        //         <div className = 'form-group inputs' onSubmit={onSubmit}>
-        //             <label><h3>Username</h3>
-        //                 <input id = 'username-input'
-        //                     value = {values.username}
-        //                     onChange = {onChange}
-        //                     name = 'username'
-        //                     type = 'text'
-        //                 />
-        //             </label>
-
-        //             <label><h3>Email</h3>
-        //                 <input id = 'email-input'
-        //                     value = {values.email}
-        //                     onChange = {onChange}
-        //                     name = 'email'
-        //                     type = 'text'
-        //                 />
-        //             </label>
-
-        //             <label><h3>Password</h3>
-        //                 <input id = 'password-input'
-        //                     value = {values.password}
-        //                     onChange = {onChange}
-        //                     name = 'password'
-        //                     type = 'text'
-        //                 />
-        //             </label>
-        //         </div>
-
-        //         <div className = 'form-group checkboxes'>
-        //             <label><h3>Accept Terms of Service</h3>
-        //                 <input id = 'terms-of-service check'
-        //                     type = 'checkbox'
-        //                     name = 'pepperoni'
-        //                     checked = {values.termsOfService}
-        //                     onChange = {onChange}
-        //                 />
-        //             </label>
-        //         </div>
-
-        //         <button>Create an Account</button>
-        //     </form>
-        // </Container>
     )
 }
