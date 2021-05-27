2486385ceff27819d1e153f4affbb2e64f77ab8e
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-05-27 15:41:11 -0500

    completed styling on potluckform and event page

--- client/src/components/PotluckPage.js
+++ client/src/components/PotluckPage.js
@@ -4,11 +4,46 @@ import { useParams } from "react-router";
 import { axiosWithAuth } from "../helps/axiosWithAuth";
 import Food from "./Food";
 
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main
+    },
+    form: {
+      width: "100%",
+      marginTop: theme.spacing(3)
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2)
+    }
+  }));
+
+const FormContainer = styled.div`
+    background-image: url('https://www.makeitgrateful.com/wp-content/uploads/2018/08/potluck-dinner-table.jpg');
+    background-repeat: no-repeat;
+    background-position: top; 
+    background-attachment: fixed;
+    background-size: cover;
+    color: #4f4f4f;
+    padding: 20%;
+`
 
 const PotluckPage = (props)=>{
     const {id} = useParams();
-
+    const classes = useStyles();
     const [potluck, setPotluck] = useState(null);
 
     useEffect(()=>{
@@ -19,27 +54,39 @@ const PotluckPage = (props)=>{
         })
     },[props.state])
 
-    return (
-        <div>{potluck &&
-            <div>
-                <h2>{`${potluck.event_name} event is taking place at ${potluck.street_address} ${potluck.city} ${potluck.state} ${potluck.zip}`}
-                    <br></br>
-                    {`Event date: ${potluck.date}`}
-                    <br></br>
-                    {`Event time: ${potluck.time}`}
-                </h2>
-                <div>
-                    {potluck.attendees.length!==0 && potluck.attendees.map(food=>{
-                        return <Food food={food}/>
-                    })}
+    return(
+        <FormContainer>
+            <Container component="main" maxWidth="sm" style={{backgroundColor: 'white', paddingTop: '.1rem', paddingBottom: '3%', paddingLeft: '3%', paddingRight: '3%', borderRadius: '3%'}}>
+                <CssBaseline />
+                    
+                <div className={classes.paper}>{potluck &&
+                    <Typography component="h2" variant="h5">
+                        {`${potluck.event_name} is taking place at`}<br></br>
+                        {potluck.street_address}<br></br>
+                        {`${potluck.city}, ${potluck.state}`}<br></br>
+                        {potluck.zip}
+                        <br></br><br></br>
+                        {`Event date: ${potluck.date}`}
+                        <br></br><br></br>
+                        {`Event time: ${potluck.time}`}
+                        {potluck.attendees.length!==0 && potluck.attendees.map(food=>{
+                            return <Food food={food}/>
+                        })}
+                    </Typography>}
+                    <form className={classes.form}>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            onClick={()=>props.history.push("/protected")}>
+                            Done
+                        </Button>
+                    </form>
                 </div>
-            </div>}
-            <button onClick={()=>props.history.push("/protected")}>Done</button>
-        </div>
-        
+            </Container>
+        </FormContainer>
     )
 }
-
 
 export default connect(state=>{
     return{state}
