d3ff9b8f888349b3e9758f2ba3a1e4e0f363ac15
alextran0899 <alextran@MacBook-Air.local>
2021-05-28 00:12:00 -0500

    image look a little busy

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,104 +1,37 @@
 import React from "react";
-import Footer from "./Footer";
-import styled from 'styled-components'
-import {useHistory} from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import ButtonTp from "@material-ui/core/Button";
+import disableScroll from 'disable-scroll'
+import styled from "styled-components";
+import { withStyles } from '@material-ui/core/styles';
+import './font.css'
+import Fab from '@material-ui/core/Fab';
+disableScroll.on()
 
+const Main = styled.div`
+    background-image: url('https://i.ibb.co/8chJYXT/http-www-pdf-tools-com.jpg');
+    background-attachment: fixed;
+    background-size: cover;
+    color: #4f4f4f;
+    padding: 20%;
+    height:100vh;
+`
+const styles = theme => ({
+    fab: {
+        margin: theme.spacing.unit,
+    },
+});
 
-
-const HomePage = ()=>{
-    const {push} = useHistory();
-
-    
-    const checkStillLogin = (e)=>{
-        e.preventDefault();
-        const token = localStorage.getItem("token");
-        if(token){
-            push("/protected");
-        }else{
-            push("/login");
-        }
-    }
-    
-    return(
-        <BodyDiv>
-            <Header>
-                <h1>Potluck Planner</h1>
-                <NavBar>
-                    <ButtonTp
-                        variant="contained"
-                        color="neutral"
-                        onClick={()=>push("/sign-up")}
-                        >
-                        Sign Up
-                    </ButtonTp>
-                    <ButtonTp
-                        variant="contained"
-                        color="primary"
-                        onClick={checkStillLogin}
-                        >
-                        Sign In
-                    </ButtonTp>
-   
-                </NavBar>
-            </Header>
-            <IntroductionDiv>
-                <h1>Happiness is eating food with friends</h1>
-            </IntroductionDiv>
-            <Footer/>
-        </BodyDiv>
+function homepage(props) {
+    const { classes } = props;
+    return (
+        <Main>
+            <h1 className="font-link">The best potluck planner <br />PERIOD.</h1>
+            <div className='buton'>
+                <Fab variant="extended" className={classes.fab}> login </Fab>
+                <Fab variant="extended" className={classes.fab}> signup </Fab>
+                <Fab variant="extended" className={classes.fab}> credit </Fab>
+            </div>
+        </Main>
     )
 }
-export default HomePage;
 
-const BodyDiv = styled.div`
-    background-color:#e1e5ea;
-    height:100%;
-`
-
-const Header = styled.header`
-    display:flex;
-    width:100%;
-    background-color: #fff5b7;
-
-    h1{
-        width:50%;
-    }
-
-`
-const NavBar = styled.nav`
-    display:flex;
-    align-items:center;
-    width:30%;
-    justify-content: space-evenly;
-
-    button{
-        width:35%;
-        height:2rem;
-    }
-`
-const IntroductionDiv = styled.div`
-    width:70%;
-    margin: 3rem auto 2rem;
-    border:2px solid silver;
-    display:flex;
-    justify-content:center;
-    padding:4rem;
-    border-radius:10px;
-`
-
-
-
-const NavLink = styled(Button)`
-padding: 5px;
-border-radius: 5px;
-background: white;
-color: black;
-text-decoration: none;
-&:hover {
-color: #ffbf80;
-}
-`;
-
-export {NavLink};
+export default withStyles(styles)(homepage);
